ac72d1ce7a84f31f5b4d7937f119d79fea6ffaf7
Mike Zhang <mike.zhang@activenetwork.com>
2018-11-21 17:35:27 +0800

    init

---
+++ aspnet-core/AspNetCoreSolution/GnericHostConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GnericHostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
