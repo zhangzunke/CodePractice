23efd56925876c6264806d375adb484cde5eeed1
Mike Zhang <mike.zhang@activenetwork.com>
2019-05-16 17:55:16 +0800

    RabbitMQ Demo

---
+++ aspnet-core/AspNetCoreSolution/RabbitMQReceive/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "10.110.120.147",
+                UserName = "admin",
+                Password = "123456"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //NewMethod(channel);
+                    channel.ExchangeDeclare(exchange: "fanoutEC", type: "fanout");
+                    var queuename = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queuename, exchange: "fanoutEC", routingKey: "");
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var message = Encoding.UTF8.GetString(ea.Body);
+                        Console.WriteLine($"B Received {message} message");
+                        Thread.Sleep(6000);
+                        Console.WriteLine("B Done");
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+                    channel.BasicConsume(queue: queuename, autoAck: false, consumer: consumer); ;
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void NewMethod(IModel channel)
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine($"B Received {message} message");
+                Thread.Sleep(6000);
+                Console.WriteLine("B Done");
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: "hello", autoAck: false, consumer: consumer);
+        }
+    }
+}
---
+++ aspnet-core/AspNetCoreSolution/RabbitMQSend/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "10.110.120.147",
+                UserName = "admin",
+                Password = "123456"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //NewMethod(args, channel);
+                    var queueName = channel.QueueDeclare().QueueName;
+                    //fanout model
+                    channel.ExchangeDeclare(exchange: "fanoutEC", type: "fanout");
+                    Console.WriteLine("Please enter your message:");
+                    while (true)
+                    {
+                        var message = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(message) && message.ToUpper() == "Q")
+                        {
+                            break;
+                        }
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "fanoutEC", routingKey: "", basicProperties: null, body: body);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This is a queue send
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="channel"></param>
+        private static void NewMethod(string[] args, IModel channel)
+        {
+            channel.QueueDeclare(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            for (int i = 0; i < 5; i++)
+            {
+                string message = args.Length > 0 ? args[0] : $"{i + 1} Hello RabbitMQ!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: properties, body: body);
+                Console.WriteLine($"A send a {message} message");
+            }
+        }
+    }
+}
